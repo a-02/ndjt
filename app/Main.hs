720157ee50c96ab759788234b8063953433c779d
nks <nks@9k.vc>
2024-01-16 11:11:33 -0600

    train-ride fixes

--- app/Main.hs
+++ app/Main.hs
@@ -41,9 +41,11 @@ init :: IO (NE.NonEmpty String)
 init = do
   putStr "A DECK IP: "
   deckA <- getLine
+{-
   putStr "B DECK IP: "
   deckB <- getLine
-  return $ NE.fromList [deckA, deckB]
+-}
+  return $ NE.fromList [deckA]
 
 main :: IO ()
 main = do
@@ -55,11 +57,13 @@ main = do
   createDirectoryIfMissing True filename
   logMainHandle <- openFile logMain WriteMode
   logNetworkHandle <- openFile logNetwork WriteMode
-  mapM_ (`hSetBuffering` NoBuffering) [logMainHandle, logNetworkHandle]
+  mapM_ (`hSetBuffering` NoBuffering) [logMainHandle, logNetworkHandle, stdout, stdin]
   decks <- Main.init
   cfg <- standardIOConfig
   vty <- mkVty cfg
+  logStringHandle logMainHandle <& ("got" ++ show decks)
   deckSockets <- mapM (`openTcp` rnsServerPort) decks
+  logStringHandle logNetworkHandle <& "successfully got deckSockets?"
   let info = NDJTInfo {vty, logMainHandle, logNetworkHandle, deckSockets}
   _ <- execRWST vtyGO info (FileLoader "")
   shutdown vty
@@ -115,6 +119,7 @@ changeMode = \case
 interpretFileLoader :: String -> Zipper (Tcp, Bool) -> Key -> App ()
 interpretFileLoader file deckActives key = do
   vty <- (.vty) <$> ask
+  drawFileLoader file deckActives
   let go x = do 
         k <- liftIO $ collapseEventToKey <$> nextEvent vty
         interpretFileLoader file x (either error id k) -- oops! got lazy
@@ -123,7 +128,6 @@ interpretFileLoader file deckActives key = do
   if key == KEnter
   then mapM_ (\(tcp, active) -> when active (load file tcp)) deckActives
   else do
-    drawFileLoader file deckActives
     case key of
       KChar ch -> put (FileLoader $ file ++ [ch])
       KBS -> put (FileLoader $ Data.List.init file) -- fix me, loser! dont use init!
@@ -139,11 +143,13 @@ drawFileLoader file deckActives = do
   let line1 = Vty.string (defAttr `withForeColor` green) file
       deckActivesShow = show $ first tcpHandle <$> deckActives
       line2 = Vty.string (defAttr `withForeColor` red) deckActivesShow
-  liftIO $ update vty (picForImage $ vertJoin line1 line2)
+      line3 = Vty.string (defAttr `withForeColor` yellow) "You are in File Loader mode."
+  liftIO $ update vty (picForImage $ foldl1 vertJoin [line1,line2,line3])
 
 interpretTreatAsBitstring :: Word256 -> Key -> App ()
 interpretTreatAsBitstring w256 key = do
   let line1 = Vty.string (defAttr `withForeColor` blue) (show w256)
+      line2 = Vty.string (defAttr `withForeColor` yellow) "You are in Bitstring mode."
   decks <- (.deckSockets) <$> ask
   vty <- (.vty) <$> ask
   liftIO $ update vty (picForImage line1)
@@ -162,10 +168,11 @@ interpretInputAsHash :: String -> Key -> App ()
 interpretInputAsHash ih key = do
   case key of 
     (KChar ch) -> do
-      let msg = ih ++ pure ch
-          adlerDigest = adler32 $ BSC8.pack msg
+      let msg = "MESSAGE --> " ++ ih ++ pure ch
+          adlerDigest = "ADLER-32 HASH --> " ++ (show . adler32 $ BSC8.pack msg)
           msgLine = Vty.string (defAttr `withForeColor` cyan) msg
           adlerLine = Vty.string (defAttr `withForeColor` magenta) (show adlerDigest)
+          statsLIne = Vty.string (defAttr `withForeColor` yellow) "You are in Adler-32 mode."
       put $ InputAsHash msg
       decks <- (.deckSockets) <$> ask
       vty <- (.vty) <$> ask
