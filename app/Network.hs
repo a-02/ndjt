be22463ea9e060dca3c0d44a393a684d4e4fffe5
nks <nks@9k.vc>
2024-05-11 23:13:04 -0500

    add todo

--- app/Network.hs
+++ app/Network.hs
@@ -15,15 +15,16 @@ xrdjPSSHClient as bs cs = mapConcurrently_ (uncurry3 sshSessionXRDJ) (zip3 as bs
 
 sshSessionXRDJ :: T.Text -> IP -> T.Text -> IO ()
 sshSessionXRDJ user ip home  =
-  withSSH2Agent "known_hosts" (T.unpack user) (T.unpack . encode $ ip) 22 (sessionAction home ip)
+  withSSH2Agent "~/.ssh/known_hosts" (T.unpack user) (T.unpack . encode $ ip) 22 (sessionAction home ip)
 
 sessionAction :: T.Text -> IP -> Session -> IO ()
 sessionAction home ip sesh = do
   let saveFilePath = "temp/digest_" ++ (T.unpack . encode $ ip)
       home' = (T.unpack home ++)
-  _ <- scpSendFile sesh 0o777 "scripts/digest" (T.unpack home)
+  _ <- scpSendFile sesh 0o777 "xras/target/release/xras" (T.unpack home)
   _ <- runShellCommands sesh ["." ++ home' "/digest"]
   _ <- scpReceiveFile sesh (home' "/out.toml") saveFilePath
   _ <- runShellCommands sesh ["rm " ++ home' "/out.toml", "rm " ++ home' "/digest" ]
   return ()
 
+--todo: dont connect to ssh when speaking to localhost
